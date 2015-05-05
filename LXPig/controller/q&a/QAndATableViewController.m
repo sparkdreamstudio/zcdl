709c7d0c943753bee1b070f46d95804310708a99
leexiang <leexiang114@gmail.com>
2015-05-06 01:23:59 +0800

    更新
    2015/5/6 你问我答

---
+++ LXPig/controller/q&a/QAndATableViewController.m
@@ -0,0 +1,138 @@
+//
+//  QAndATableViewController.m
+//  LXPig
+//
+//  Created by leexiang on 15/5/6.
+//
+//
+
+#import "QAndATableViewController.h"
+#import "NetWorkClient.h"
+@interface QAndATableViewController ()
+@property (nonatomic,strong) NSMutableArray * problemArray;
+@property (assign,nonatomic) NSInteger currentPage;
+@end
+
+@implementation QAndATableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self addPullRefresh];
+    [self addInfinitScorll];
+    [self startRefresh];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+-(void)pullRfresh
+{
+    self.currentPage = 1;
+    NSMutableDictionary* params = [NSMutableDictionary dictionary];
+    [params setValue:@"list" forKey:@"action"];
+    [params setValue:@"10" forKey:@"pageSize"];
+    [params setValue:[NSNumber numberWithInteger:self.currentPage] forKey:@"currentPageNo"];
+    [params setValue:[[UserManagerObject shareInstance]sessionid] forKey:@"sessionid"];
+    [[NetWorkClient shareInstance]postUrl:SERVICE_PROBLEM With:params success:^(NSDictionary *responseObj, NSString *timeSp) {
+        [self stopPull];
+        self.problemArray = [NSMutableArray arrayWithArray:[responseObj objectForKey:@"data"]];
+        if (self.problemArray.count <10) {
+            [self setInfinitScorllHidden:YES];
+        }
+        else
+        {
+            [self setInfinitScorllHidden:NO];
+        }
+        [self.tableView reloadData];
+    } failure:^(NSDictionary *responseObj, NSString *timeSp) {
+        [self stopPull];
+    }];
+}
+
+-(void)infinitScorll
+{
+    self.currentPage++;
+    NSMutableDictionary* params = [NSMutableDictionary dictionary];
+    [params setValue:@"list" forKey:@"action"];
+    [params setValue:@"10" forKey:@"pageSize"];
+    [params setValue:[NSNumber numberWithInteger:self.currentPage] forKey:@"currentPageNo"];
+    [params setValue:[[UserManagerObject shareInstance]sessionid] forKey:@"sessionid"];
+    [[NetWorkClient shareInstance]postUrl:SERVICE_PROBLEM With:params success:^(NSDictionary *responseObj, NSString *timeSp) {
+        [self stopPull];
+        NSMutableArray* array = [NSMutableArray arrayWithArray:[responseObj objectForKey:@"data"]];
+        if(array.count == 0)
+        {
+            [self setInfinitScorllHidden:YES];
+            return ;
+        }
+        if (array.count <10) {
+            [self setInfinitScorllHidden:YES];
+        }
+        else
+        {
+            [self setInfinitScorllHidden:NO];
+        }
+        [self.problemArray addObjectsFromArray:array];
+        NSMutableArray* indexPathArray = [NSMutableArray array];
+        for (NSInteger index = (self.problemArray.count - array.count -1); index < self.problemArray.count; index++) {
+            [indexPathArray addObject:[NSIndexPath indexPathForRow:index inSection:0]];
+        }
+        
+        [self.tableView beginUpdates];
+        [self.tableView insertRowsAtIndexPaths:indexPathArray withRowAnimation:UITableViewRowAnimationAutomatic];
+        [self.tableView endUpdates];
+    } failure:^(NSDictionary *responseObj, NSString *timeSp) {
+        [self stopPull];
+        self.currentPage--;
+    }];
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.problemArray.count;
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 153;
+}
+
+-(UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell* cell = nil;
+    NSDictionary* dic = self.problemArray[indexPath.row];
+    NSInteger isSolve = [dic[@"isSolve"] integerValue];
+    if (isSolve == 0) {
+        cell = [tableView dequeueReusableCellWithIdentifier:@"cell1" forIndexPath:indexPath];
+    }
+    else
+    {
+        cell = [tableView dequeueReusableCellWithIdentifier:@"cell0" forIndexPath:indexPath];
+    }
+    UILabel* label = (UILabel*)[cell viewWithTag:1];
+    label.text = [NSString stringWithFormat:@"%@提问",dic[@"members"][@"name"]];
+    label = (UILabel*)[cell viewWithTag:2];
+    label.text = dic[@"content"];
+    label = (UILabel*)[cell viewWithTag:3];
+    label.text = dic[@"createTime"];
+    label = (UILabel*)[cell viewWithTag:4];
+    label.text = [NSString stringWithFormat:@"有用 %@ | 回复 %@",dic[@"usefulCnt"],dic[@"replyCnt"]];
+    return cell;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
